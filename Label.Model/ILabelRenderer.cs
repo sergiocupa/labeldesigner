e77860cb24458224e859605cfda3bb669b520e6e
sergiocupa <76142496+sergiocupa@users.noreply.github.com>
2021-05-12 00:37:10 -0300

    Add files via upload

---
+++ Label.Model/ILabelRenderer.cs
@@ -0,0 +1,36 @@
+﻿
+
+using System.Drawing;
+
+
+namespace Label.Model
+{
+
+    public interface ILabelRenderer
+    {
+
+        Font GetPrinterFont(string font_name, double size);
+
+    }
+
+
+    public interface ILabelAssembler
+    {
+
+        string AssembleLabel(PrintLabel label);
+        string AssembleLabel(PrintLabel label, object data);
+
+    }
+
+
+    public enum LabelProtocol
+    {
+        UNKNOWN = 0,
+        ZPL2    = 1,
+        EPL     = 2
+    }
+
+
+  
+
+}
